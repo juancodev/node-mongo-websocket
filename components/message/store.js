82dbc23057b81d90bef6cd1d6a214fd90666330e
juancodev <montillasanchezjuancarlos@gmail.com>
2023-07-05 14:07:50 -0400

    [⚡ADD] creating schema

--- components/message/store.js
+++ components/message/store.js
@@ -1,13 +1,28 @@
-//Mock
+const db = require('mongoose');
+const Model = require('./model');
 
-const list = [];
+require('dotenv').config();
+
+const hostDB = process.env.DB_HOST;
+const userDB = process.env.DB_USER;
+const passwordDB = process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://${userDB}:${passwordDB}@${hostDB}/?retryWrites=true&w=majority`;
+
+db.Promise = global.Promise;
+db.connect(uri, {
+  useNewUrlParser: true,
+});
+console.log('[db] Connected to mongodb');
 
 function addMessage(message) {
-  list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 };
 
-function getMessage() {
-  return list;
+async function getMessage() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
